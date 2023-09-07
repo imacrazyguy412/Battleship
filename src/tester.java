afc772107b870c9d4fc96658b268d6de2c86936a
imacrazyguy412 <110923269+imacrazyguy412@users.noreply.github.com>
2023-09-07 10:27:39 -0400

    idk just todays stuff

--- src/tester.java
+++ src/tester.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class tester {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		String input;
 		/*Position pos1 = new Position("A", 1);
 		System.out.println(pos1.row());
 		System.out.println(pos1.column());
@@ -20,18 +21,42 @@ public class tester {
 		
 		
 
+		
+
+
+		//MUY IMPORTANTE REENABLE LATER PLEASE
+		/*
 		System.out.println("what class would you like to test?");
 		String input = scan.nextLine();
+		//Boat testing
 		if(input.equals("Boat")){
-			System.out.println("enter boat info");
-		} else if(input.equals("Position")){
+			System.out.println("enter boat info:");
+			
+
+		} 
+		//position testing
+		else if(input.equals("Position")){
+
 			System.out.println("input a positinon");
+			input = scan.nextLine();
 			while(input != "done") {
 			System.out.println(PositionChecker.checkPosition(input));
 			input = scan.nextLine();
 			}
 		}
-		
+		 */
+		System.out.println("done to exit");
+		//input type
+		System.out.println("input type: Aircraft Carrier, Battleship, Cruiser, Submarine, Destroyer");
+		String type = scan.nextLine();
+		//input position of boat (starting from top left)
+		System.out.println("input a position in letter-number form");
+		Position pos = PositionChecker.checkPosition(scan.nextLine());
+		//input orientation of boat
+		System.out.println("input an orientation: Vertical, Horizontal");
+		String orientation = scan.nextLine();
+		Boat boat = new Boat(type, pos, orientation);
+
 		
 		
 	}
