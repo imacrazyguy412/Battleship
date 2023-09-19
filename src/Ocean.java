e1fa2df4702be7ca3d71870e183532e4f3500878
imacrazyguy412 <110923269+imacrazyguy412@users.noreply.github.com>
2023-09-19 10:01:59 -0400

    Revert "broke everything"
    This reverts commit fb57c68aa2d073df3ad08c60477ebfe6ce08a9a0.

--- src/Ocean.java
+++ src/Ocean.java
@@ -1,17 +1,16 @@
 import exceptions.BoatOutOfBoundsException;
 import exceptions.BoatOverlappingException;
-import java.util.ArrayList;
 
 public class Ocean {
-    private ArrayList<Boat> boats;
+    private Boat[] boats;
 
 
     public Ocean(){
-        boats = new ArrayList<Boat>();
+        
     }
 
 
-    public void placeBoat(String boatName, String direction, Position pos) throws BoatOutOfBoundsException, BoatOverlappingException {
+    public void placeBoat(String boatName, String direction, Position pos) throws Exception {
         Boat testBoat = new Boat(boatName, pos, direction);
 
         //check for overlapping boats
@@ -19,40 +18,31 @@ public class Ocean {
             //vertical check
             if(direction.equals("Vertical")){
                 for(int i = 0; i<testBoat.size(); i++){
-                    if(b.onBoat(new Position(pos.colIndex()+1, pos.rowIndex() + i + 1))){
+                if(b.onBoat(new Position(pos.colIndex()+1, pos.rowIndex() + i + 1))){
+                    System.out.println("overlapping boats");
                     throw(new BoatOverlappingException());
-                    }
+                }
                 }
             }
             if(direction.equals("Horizontal")){
                 for(int i = 0; i<testBoat.size(); i++){
                     if(b.onBoat(new Position(pos.rowIndex()+1, pos.colIndex() + i + 1))){
+                        System.out.println("overlapping boats");
                         throw(new BoatOverlappingException());
                     }
                 }
             }
         }
-        
-        System.out.println("RUN THE EXCEPTION BRO");
+
         //check for boats out of bounds
-        if(direction.equals("Vertical")){
-            System.out.println("test 1 true");
-            System.out.println(pos.colIndex());
-            System.out.println(pos.rowIndex());
-   
+        if(direction.equals("Vertical") && pos.row() + testBoat.size() >= 10){
+
         }
-        if(direction.equals("Vertical") && pos.colIndex() + testBoat.size() >= 10){
-            System.out.println("uh hello please eception me");
-            throw(new BoatOutOfBoundsException());
-        }
-        else if(direction.equals("Horizontal") && pos.rowIndex() + testBoat.size() >= 10){
-            throw(new BoatOutOfBoundsException());
-        }
-        boats.add(testBoat);
+        boats[boats.length] = testBoat;
         //throw(new Exception());
     }
 
-    //loop through boats and use onBoat with pos on each boat, return boat method if true 
+
     /* 
     public void shootAt(Position pos){
 
@@ -78,13 +68,4 @@ public class Ocean {
 
     }
     */
-
-    public String toString(){
-        String out = "";
-        for(Boat b : boats){
-            out += "name: " + b.name() + "\nabbreviation: " + b.abbreviation() + "\ndirection:  " + b.direction() + "\nposition: " + b.position();
-            out += "\n\n";
-        }
-        return out;
-    }
 }
--- src/tester.java
+++ src/tester.java
@@ -1,9 +1,6 @@
 
 import java.text.ParsePosition;
 import java.util.Scanner;
-
-import exceptions.BoatOutOfBoundsException;
-import exceptions.BoatOverlappingException;
 public class tester {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -30,7 +27,7 @@ public class tester {
 
 		//MUY IMPORTANTE REENABLE LATER PLEASE
 		
-		System.out.println("what class would you like to test?\nBoat, Position, Ocean");
+		System.out.println("what class would you like to test?\nBoat, Position");
 		input = scan.nextLine();
 		//Boat testing
 		if(input.equals("Boat")){
@@ -84,26 +81,6 @@ public class tester {
 			System.out.println(PositionChecker.checkPosition(input));
 			input = scan.nextLine();
 			}
-		} 
-		//ocean testiing
-		else if(input.equals("Ocean")){
-			Ocean ocean1 = new Ocean();
-			try{
-				ocean1.placeBoat("Aircraft Carrier", "Vertical", PositionChecker.checkPosition("a-1"));
-			} catch(BoatOutOfBoundsException e){
-				System.out.println("Boat out of bounds");
-			} catch (BoatOverlappingException e){
-				System.out.println("Boat overlapping other boat");
-			}
-			try{
-				ocean1.placeBoat("Battleship", "Vertical", PositionChecker.checkPosition("a-9"));
-			} catch(BoatOutOfBoundsException e){
-				System.out.println("Boat out of bounds");
-			} catch (BoatOverlappingException e){
-				System.out.println("Boat overlapping other boat");
-			}
-
-			System.out.println(ocean1);
 		}
 		 
 		/*
